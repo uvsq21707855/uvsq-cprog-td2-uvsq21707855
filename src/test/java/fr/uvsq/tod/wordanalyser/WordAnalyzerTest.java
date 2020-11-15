63f5f88d397dea5889fe40000583fc315c2bf419
uvsq21707855 <koceilawarda@gmail.com>
2020-11-15 20:38:06 +0100

    test first_repeated_character

--- src/test/java/fr/uvsq/tod/wordanalyser/WordAnalyzerTest.java
+++ src/test/java/fr/uvsq/tod/wordanalyser/WordAnalyzerTest.java
@@ -2,11 +2,17 @@ package fr.uvsq.tod.wordanalyser;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class WordAnalyzerTest {
   @Test
   public void shouldTest() {
     fail("Not yet implemented.");
+  }
+
+  public void firstRepeatedCharactertest() {
+    WordAnalyzer w = new WordAnalyzer("maan");
+    assertEquals('a', w.firstRepeatedCharacter());
   }
 }
